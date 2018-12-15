a32ad91518f22dce4521fd5c9f6475d770cfe174
phansonloc1999 <phansonloc1999@gmail.com>
2018-12-15 10:57:57 +0700

    Add fall(), stop(), rotate()

--- src/Tetrominoes/BaseTetromino.lua
+++ src/Tetrominoes/BaseTetromino.lua
@@ -1,13 +1,8 @@
-BaseTetromino = Class {}
+---@class BaseTetromino
+BaseTetromino = Class{}
 
-MAPPINGS = {
-    {
-        {1, 1, 0, 0},
-        {0, 1, 0, 0},
-        {0, 1, 0, 0},
-        {0, 0, 0, 0}
-    }
-}
+local maxFallingTime = 0.5
+local fallingTimer = maxFallingTime
 
 ------------Core functions---------------
 function BaseTetromino:init(x, y, MAPPINGS)
@@ -17,7 +12,7 @@ function BaseTetromino:init(x, y, MAPPINGS)
     self._mappingIndex = 1
     self._stopped = false
 
-    self:useMapping(MAPPINGS[1])
+    self:useMapping(MAPPINGS)
 end
 
 function BaseTetromino:render()
@@ -31,7 +26,12 @@ function BaseTetromino:render()
 end
 
 function BaseTetromino:update(dt)
-    self:move()
+    if (not self._stopped) then
+        self:move()
+        self:rotate()
+        self:fall(dt)
+        self:stop()
+    end
 end
 -----------------------------------------
 function BaseTetromino:move()
@@ -49,6 +49,7 @@ function BaseTetromino:move()
 end
 
 function BaseTetromino:moveLeft()
+    self._x = self._x - BLOCK_WIDTH
     for i = 1, 4 do
         for j = 1, 4 do
             if (self._blocks[i][j]) then
@@ -59,6 +60,7 @@ function BaseTetromino:moveLeft()
 end
 
 function BaseTetromino:moveRight()
+    self._x = self._x + BLOCK_WIDTH
     for i = 1, 4 do
         for j = 1, 4 do
             if (self._blocks[i][j]) then
@@ -68,7 +70,72 @@ function BaseTetromino:moveRight()
     end
 end
 
-function BaseTetromino:rotate()
+function BaseTetromino:rotate(MAPPINGS)
+    if (love.keyboard.wasPressed("space")) then
+        if (self._mappingIndex < 4) then
+            self._mappingIndex = self._mappingIndex + 1
+        else
+            self._mappingIndex = 1
+        end
+        self:useMapping(MAPPINGS)
+
+        if (not self:checkBlocksPosValidity()) then
+            if (self._mappingIndex == 1) then
+                self._mappingIndex = 4
+            else
+                self._mappingIndex = self._mappingIndex - 1
+            end
+            self:useMapping(MAPPINGS)
+        end
+    end
+end
+
+function BaseTetromino:fall(dt)
+    if (fallingTimer == 0) then
+        self._y = self._y + BLOCK_HEIGHT
+        for i = 1, 4 do
+            for j = 1, 4 do
+                if (self._blocks[i][j]) then
+                    self._blocks[i][j]:fall()
+                end
+            end
+        end
+        fallingTimer = maxFallingTime
+    else fallingTimer = math.max(0, fallingTimer - dt) end
+end
+
+function BaseTetromino:stop()
+    for i = 1, 4 do
+        for j = 1, 4 do
+            if (self._blocks[i][j]) then
+                if (self._blocks[i][j]._y >= 225) then
+                    self._stopped = true
+                    return
+                end
+            end
+        end
+    end
+end
+
+-----------------------------------------
+function BaseTetromino:createBlockAt(row, column)
+    local x, y = self._x + (column - 1) * BLOCK_WIDTH, self._y + (row - 1) * BLOCK_HEIGHT
+    if (self._blocks[row] == nil) then
+        self._blocks[row] = {}
+    end
+    self._blocks[row][column] = Block(x, y)
+end
+
+function BaseTetromino:useMapping(MAPPINGS)
+    self._blocks = {{}, {}, {}, {}}
+    self._mapping = MAPPINGS[self._mappingIndex]
+    for i = 1, 4 do
+        for j = 1, 4 do
+            if (self._mapping[i][j] == 1) then
+                self:createBlockAt(i, j)
+            end
+        end
+    end
 end
 
 function BaseTetromino:checkBlocksPosValidity()
@@ -81,24 +148,4 @@ function BaseTetromino:checkBlocksPosValidity()
         end
     end
     return true
-end
------------------------------------------
-function BaseTetromino:createBlockAt(row, column)
-    local x, y = self._x + (column - 1) * BLOCK_WIDTH, self._y + (row - 1) * BLOCK_HEIGHT
-    if (self._blocks[row] == nil) then
-        self._blocks[row] = {}
-    end
-    self._blocks[row][column] = Block(x, y)
-end
-
-function BaseTetromino:useMapping(MAPPING)
-    self._blocks = {{}, {}, {}, {}}
-    self._mapping = MAPPING[self._mappingIndex]
-    for i = 1, 4 do
-        for j = 1, 4 do
-            if (MAPPING[i][j] == 1) then
-                self:createBlockAt(i, j)
-            end
-        end
-    end
 end
